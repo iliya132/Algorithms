c78fa08f3f2e5fefe9001c4eb3779826316b2926
Ilya132@live.com <iliya132@live.com>
2021-06-04 10:15:11 +0300

    Добавлен бинарный поиск

---
+++ Algorithms/Search/BinarySearch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Search
+{
+    public class BinarySearch : ISearch
+    {
+        public int? Search(int[] array, int value)
+        {
+            int low = 0;
+            int high = array.Length;
+            
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+                if (array[mid] == value)
+                {
+                    return mid;
+                }else if(array[mid] > value)
+                {
+                    high = mid - 1;
+                }else if(array[mid] < value)
+                {
+                    low = mid + 1;
+                }
+            }
+            return null;
+        }
+    }
+}
